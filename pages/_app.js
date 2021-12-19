50481128822c18447db14c0af738e1a75e9bb5a0
Iseul Park <lovelyangelapark@gmail.com>
2021-12-19 19:54:31 +0900

    seultube setup

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import '../styles/globals.css'
+
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+
+export default MyApp
+
+
+// import '../styles/globals.css'
+// // import AppLayout from "../components/AppLayout";
+// import Head from "next/head";
+
+// function MyApp({ Component, pageProps }) {
+//   return( 
+//   // <Component {...pageProps} />
+ 
+//       <>
+//       <Head>
+//         <title>SNS</title>
+//       </Head>
+//         <Component {...pageProps} />
+//     </>
+//   )
+// }
+
+// export default MyApp;
